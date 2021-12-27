9fc36c39f71b57990c84ddcd14c7cbd2ba36a100
hauchongtang <56034480+hauchongtang@users.noreply.github.com>
2021-12-27 16:21:29 +0800

    initial commit

---
+++ DynamicProgramming/LongestPalindromicSubstring.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        int left = 0;
+        int right = 0;
+        int max = 1;
+
+        for (int i = 0; i < s.length(); i++) {
+            int l = i, r = i;
+            while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+                l--;
+                r++;
+            }
+            if (r - l > max) {
+                left = l + 1;
+                right = r;
+                max = r - l;
+            }
+        }
+        
+        for (int i = 0; i < s.length(); i++) {
+            int l = i, r = i+1;
+            while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+                l--;
+                r++;
+            }
+            if (r - l > max) {
+                left = l + 1;
+                right = r;
+                max = r - l;
+            }
+        }
+        
+        String result = s.substring(left, right);
+        return result.equals("") ? s.substring(0,1) : result;
+    }
+}
